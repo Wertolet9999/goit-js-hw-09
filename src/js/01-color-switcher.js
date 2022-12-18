e5da8fe16d80dd66196dca6c68882e109ffb5dcf
Wertolet9999 <wertolet9999@gmail.com>
2022-12-18 17:37:26 +0200

    add first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
-
+function getRandomHexColor() {
+   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const bodyEl = document.querySelector('body');
+const startBtnEl = document.querySelector('button[data-start]');
+const stopBtnEl = document.querySelector('button[data-stop]');
+let timerId = null;
+startBtnEl.addEventListener('click', onStartBtn);
+stopBtnEl.addEventListener('click',  onstopBtnEl);
+function onStartBtn() {
+   timerId = setInterval(() => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+   }, 1000);
+  startBtnEl.setAttribute('disabled', 'disabled');
+}
+function onstopBtnEl() {
+  clearInterval(timerId);
+  startBtnEl.removeAttribute('disabled', 'disabled');
+}
